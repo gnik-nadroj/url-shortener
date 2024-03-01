921244e85e6274f430e57454e02011b0a1c6c503
gnik-nadroj <sullyjake718@gmail.com>
2024-03-01 16:21:06 +0100

    add increment

--- server/service/redirect.go
+++ server/service/redirect.go
@@ -15,5 +15,13 @@ func redirect(c *gin.Context, s *data_access.URLStore) {
 		c.JSON(http.StatusNotFound, gin.H{"error": "URL not found"})
 		return
 	}
+
+	err = s.IncrementClicksCount(shortURL)
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "An error occur"})
+		return
+	}
+
 	c.Redirect(http.StatusMovedPermanently, originalURL)
 }
